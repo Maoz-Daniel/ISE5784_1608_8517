a167557c3c84a29e9426f2c498bd3f183c1717fa
Maoz-Daniel <maoz2231@gmail.com>
2024-05-07 17:28:55 +0300

    first commit

---
+++ src/primitives/Point.java
@@ -0,0 +1,18 @@
+package primitives;
+
+public class Point {
+    /**
+     * *
+     *
+     */
+    protected Double3 xyz;
+
+    /**
+     * recives a point and ...
+     * @param p
+     * @return
+     */
+    public double distance(Point p) {
+        return 0.0;
+    }
+}
