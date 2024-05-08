7a35f1a0eda22d07c5adb29a81e5606f36d74676
Maoz-Daniel <maoz2231@gmail.com>
2024-05-08 20:05:57 +0300

    lol

---
+++ src/geometries/Plane.java
@@ -0,0 +1,28 @@
+package geometries;
+import primitives.Point;
+import primitives.Vector;
+
+public class Plane {
+    private Point q;
+    private Vector normal;
+
+    public Plane(Point p1, Point p2, Point p3) {
+         normal = (p2.subtract(p1)).crossProduct(p3.subtract(p1)).normalize();
+        q=p1;
+    }
+
+    public Plane(Point q, Vector normal) {
+        this.q = q;
+        this.normal = normal.normalize();
+    }
+
+    public Vector getNormal(Point point) {
+
+            return normal;
+    }
+
+    public Vector getNormal() {
+        return normal;
+
+    }
+}
