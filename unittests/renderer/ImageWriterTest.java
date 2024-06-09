2d0a2c915bb4441ba247a8d2ec7f69e69dc15858
Maoz-Daniel <maoz2231@gmail.com>
2024-06-09 21:47:13 +0300

    Create the new image OMGGGG

---
+++ unittests/renderer/ImageWriterTest.java
@@ -0,0 +1,32 @@
+package renderer;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+import primitives.*;
+ import static java.awt.Color.BLUE;
+import static java.awt.Color.ORANGE;
+
+
+
+
+public class ImageWriterTest {
+
+    @Test
+    public void testWriteToImageTest() {
+        ImageWriter imageWriter = new ImageWriter("test", 800, 500);
+        Color Orange = new Color(ORANGE);
+        Color Blue = new Color(BLUE);
+
+        for (int i = 0; i < 800; i++)
+            for (int j = 0; j < 500; j++)
+                imageWriter.writePixel(i, j, i % 50 == 0 || j % 50 == 0 ? Blue : Orange); // 16x10 grid
+        imageWriter.writeToImage();
+    }
+
+
+
+
+
+
+
+}
